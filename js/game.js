72ff370bf2ed41a5772a8067dd8ae1ebbe13661f
Stefan de Gier <99040150@mydavinci.nl>
2016-11-23 09:56:04 +0100

    Plaatjes javascript geadd

--- js/game.js
+++ js/game.js
@@ -1,55 +1,50 @@
 // javascript library for simple game development
 
-document.getElementById('option1').innerHTML = 'Dit is keuze 1';
-document.getElementById('level_title').innerHTML = 'Start het spel';
-
-var HasKey = false;
-
-// jquery variant
-//$('#option1').html('Dit is keuze 1');
-
-
-// https://www.youtube.com/watch?v=4ba1BqJ4S2M
+    document.getElementById('option1').innerHTML = 'Start';
+    document.getElementById('option2').style.display = "none"
+	document.getElementById('option3').style.display = "none"
+	document.getElementById('gameover').style.display = "none"
+    
 
 
 
 function Level1() {
 	console.log("Level1()");
+	
 
 	var opt1 = document.getElementById('option1');
-	opt1.innerHTML = 'Level 1: optie 1';
+	opt1.innerHTML = 'Ga de grot in ';
 	opt1.setAttribute("onClick", "javascript:Level2();");
 
-	document.getElementById('level_title').innerHTML = 'Level 1';
-	document.getElementById('level_image').src = 'img/level1.jpg';
+	document.getElementById('level_title').innerHTML = 'Cave';
+	document.body.style.backgroundImage = "url('cave.jpg')";
 
 
-	// only allow option 2 if user has a key in his inventory
-
-	var opt2 = document.getElementById('option2');
-	opt2.innerHTML = 'Level 1: optie 1';
-	opt2.onclick = function(){
-		if(HasKey){
-			Level2();
-		} else {
-			alert('U dont haz key dumbass');
-		}
-	}
-
-	var opt3 = document.getElementById('option3');
-
-	opt3.onclick = function(){
-		HasKey = true;
-	}
 }
+
 
 function Level2() {
 	console.log("Level2()");
 
 	var opt1 = document.getElementById('option1');
-	opt1.innerHTML = 'Level 2: optie 1';
-	opt1.onClick = 'javascript:Level3();';
+	opt1.innerHTML = 'Test';
+	opt1.setAttribute("onClick", "javascript:Level3();");
 
-	document.getElementById('level_title').innerHTML = 'Level 2';
-	document.getElementById('level_image').src = 'img/level2.jpg';
+	var opt2 = document.getElementById('option2');
+	opt2.innerHTML = '100';
+	opt2.setAttribute("onClick", "javascript:Level3();");
+
+	var opt3 = document.getElementById('option3');
+	opt3.innerHTML = '100';
+	opt3.setAttribute("onClick", "javascript:Level3();");
+
+	var opt1 = document.getElementById('gameover');
+	optgameover.innerHTML = '100';
+	optgameover.setAttribute("onClick", "javascript:gameover();");
+
+	document.getElementById('level_title').innerHTML = 'Een gat in de grond';
+	document.body.style.backgroundImage = "url('hole.jpg')";
+
+
 }
+
