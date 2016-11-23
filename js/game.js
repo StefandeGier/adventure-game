8a0fdbfae55de2b892d30f5813536dbea3d90a4a
Stefan de Gier <99040150@mydavinci.nl>
2016-11-23 14:25:00 +0100

    Nieuwe dingen

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 // javascript library for simple game development
 
+
     document.getElementById('option1').innerHTML = 'Start';
     document.getElementById('option2').style.display = "none"
 	document.getElementById('option3').style.display = "none"
@@ -10,7 +11,9 @@
 
 function Level1() {
 	console.log("Level1()");
-	
+	document.getElementById('option2').style.display = "none"
+	document.getElementById('option3').style.display = "none"
+	document.getElementById('gameover').style.display = "none"
 
 	var opt1 = document.getElementById('option1');
 	opt1.innerHTML = 'Ga de grot in ';
@@ -27,24 +30,42 @@ function Level2() {
 	console.log("Level2()");
 
 	var opt1 = document.getElementById('option1');
-	opt1.innerHTML = 'Test';
-	opt1.setAttribute("onClick", "javascript:Level3();");
+	opt1.innerHTML = 'Springen';
+	opt1.setAttribute("onClick", "javascript:GAMEOVER();");
 
 	var opt2 = document.getElementById('option2');
-	opt2.innerHTML = '100';
+	opt2.innerHTML = 'Touw';
 	opt2.setAttribute("onClick", "javascript:Level3();");
+	document.getElementById('option2').style.display = "inline"
 
-	var opt3 = document.getElementById('option3');
-	opt3.innerHTML = '100';
-	opt3.setAttribute("onClick", "javascript:Level3();");
+    var opt3 = document.getElementById('option3');
+	opt3.innerHTML = 'Walrunnen';
+	opt3.setAttribute("onClick", "javascript:GAMEOVER();");
+	document.getElementById('option3').style.display = "inline"	
+	
 
-	var opt1 = document.getElementById('gameover');
-	optgameover.innerHTML = '100';
-	optgameover.setAttribute("onClick", "javascript:gameover();");
+	
 
 	document.getElementById('level_title').innerHTML = 'Een gat in de grond';
-	document.body.style.backgroundImage = "url('hole.jpg')";
+	document.body.style.backgroundImage = "url('gat.png')";
 
 
+}
+
+function GAMEOVER(){
+	console.log("GAMEOVER");
+	
+	document.body.style.backgroundImage = "url('gameover.png')";
+	document.getElementById('level_title').innerHTML = 'Je bent af...';
+	document.getElementById('option1').style.display = "none"
+	document.getElementById('option2').style.display = "none"
+	document.getElementById('option3').style.display = "none"
+	document.getElementById('gameover').style.display = "none"
+
+    var opt3 = document.getElementById('GAMEOVER');
+	opt3.innerHTML = 'Gameover';
+	opt3.setAttribute("onClick", "javascript:index.html();");
+
+	
 }
 
