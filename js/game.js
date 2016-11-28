e525d4e12c1cd00bf60f4c059625374a00ee646a
Stefan de Gier <99040150@mydavinci.nl>
2016-11-28 08:42:28 +0100

    v1.4 buttons fade en levels

--- js/game.js
+++ js/game.js
@@ -6,8 +6,10 @@
 	document.getElementById('option3').style.display = "none"
 	document.getElementById('opnieuw').style.display = "none"
     document.getElementById('touw').style.display = "none"
-    Haskey = false;
+    document.getElementById('zak').style.display = "none"
 
+    Haskey = false;
+    Zak = false;
     
 
 function GAMEOVER(){
@@ -18,6 +20,7 @@ function GAMEOVER(){
 	document.getElementById('option2').style.display = "none"
 	document.getElementById('option3').style.display = "none"
 	document.getElementById('touw').style.display = "none"
+	document.getElementById('zak').style.display = "none"
 	document.getElementById('opnieuw').style.display = "inline"
 
 }
@@ -33,6 +36,7 @@ function Level1() {
 	document.getElementById('option2').style.display = "none"
 	document.getElementById('option3').style.display = "none"
 	document.getElementById('opnieuw').style.display = "none"
+	document.getElementById('zak').style.display = "none"
 	
 
 	var opt1 = document.getElementById('option1');
@@ -52,6 +56,7 @@ document.getElementById('touw').style.display = "inline"
     document.getElementById('level_title').innerHTML = 'Een gat in de grond';
 	document.body.style.backgroundImage = "url('gat.png')";
 	document.getElementById('opnieuw').style.display = "none"
+	document.getElementById('zak').style.display = "none"
 
 
 	var opt1 = document.getElementById('option1');
@@ -78,12 +83,158 @@ var touw = document.getElementById('touw')
 
 touw.onclick = function(){
 	Haskey = true;
+	document.getElementById("touw").style.visibility="hidden";
 }
 }
 
 function level3() {
     
 	console.log("Level3")
+	document.getElementById('option1').style.display = "inline"
+	document.getElementById('option2').style.display = "inline"
+	document.getElementById('option3').style.display = "inline"
+	document.getElementById('opnieuw').style.display = "none"
+    document.getElementById('touw').style.display = "none"
+    document.getElementById('zak').style.display = "none"
+
+	document.getElementById('level_title').innerHTML = 'Next test'
+	document.body.style.backgroundImage = "url('nietsnor.png')"
+
+var opt1 = document.getElementById('option1');
+	opt1.innerHTML = 'Springen';
+	opt1.setAttribute("onClick", "javascript:level4();");
+
+var opt2 = document.getElementById('option2');
+	opt2.innerHTML = 'Springen';
+	opt2.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt3 = document.getElementById('option3');
+	opt3.innerHTML = 'Springen';
+	opt3.setAttribute("onClick", "javascript:GAMEOVER();");
+}
+function level4() {
+    
+	console.log("Level4")
+	document.getElementById('option1').style.display = "inline"
+	document.getElementById('option2').style.display = "inline"
+	document.getElementById('option3').style.display = "inline"
+	document.getElementById('opnieuw').style.display = "none"
+    document.getElementById('touw').style.display = "none"
+    document.getElementById('zak').style.display = "none"
+
+	document.getElementById('level_title').innerHTML = 'Next test'
+	document.body.style.backgroundImage = "url('discover.png')"
+
+var opt1 = document.getElementById('option1');
+	opt1.innerHTML = 'Springen';
+	opt1.setAttribute("onClick", "javascript:level5();");
+
+var opt2 = document.getElementById('option2');
+	opt2.innerHTML = 'Springen';
+	opt2.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt3 = document.getElementById('option3');
+	opt3.innerHTML = 'Springen';
+	opt3.setAttribute("onClick", "javascript:GAMEOVER();");
+}
+
+function level5() {
+    
+	console.log("Level5")
+	document.getElementById('option1').style.display = "inline"
+	document.getElementById('option2').style.display = "inline"
+	document.getElementById('option3').style.display = "inline"
+	document.getElementById('opnieuw').style.display = "none"
+    document.getElementById('touw').style.display = "none"
+    document.getElementById('zak').style.display = "none"
+
+	document.getElementById('level_title').innerHTML = 'Klik Next'
+	document.body.style.backgroundImage = "url('ijwalkgood.jpg.png')"
+
+var opt1 = document.getElementById('option1');
+	opt1.innerHTML = 'Springen';
+	opt1.setAttribute("onClick", "javascript:level6();");
+
+var opt2 = document.getElementById('option2');
+	opt2.innerHTML = 'Springen';
+	opt2.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt3 = document.getElementById('option3');
+	opt3.innerHTML = 'Springen';
+	opt3.setAttribute("onClick", "javascript:GAMEOVER();");
+}
+function level6() {
+    
+	console.log("Level6")
+	document.getElementById('option1').style.display = "none"
+	document.getElementById('option2').style.display = "none"
+	document.getElementById('option3').style.display = "none"
+	document.getElementById('opnieuw').style.display = "none"
+    document.getElementById('touw').style.display = "none"
+    document.getElementById('zak').style.display = "inline"
+
+	document.getElementById('level_title').innerHTML = 'See idol'
+	document.body.style.backgroundImage = "url('ijstare.jpg')"
+
+var opt1 = document.getElementById('option1');
+	opt1.innerHTML = 'Springen';
+	opt1.setAttribute("onClick", "javascript:GAMEOVER();");
+
+document.getElementById('option2').style.display = "inline"
+	var opt2 = document.getElementById('option2');
+	opt2.innerHTML = 'Zak';
+	opt2.onclick = function(){
+		if (Zak) {
+			level7();
+			
+	} else{
+		alert('Geen zak!')
+	}
+}
+
+var zak = document.getElementById('zak')
+
+zak.onclick = function(){
+	Zak = true;
+	
+     document.getElementById("zak").style.visibility="hidden";
+   
+}
+
+
+var opt3 = document.getElementById('option3');
+	opt3.innerHTML = 'Springen';
+	opt3.setAttribute("onClick", "javascript:GAMEOVER();");
+
+}
+
+function level7() {
+    
+	console.log("Level7")
+	document.getElementById('option1').style.display = "inline"
+	document.getElementById('option2').style.display = "inline"
+	document.getElementById('option3').style.display = "inline"
+	document.getElementById('opnieuw').style.display = "none"
+    document.getElementById('touw').style.display = "none"
+
+	document.getElementById('level_title').innerHTML = 'Next test'
+	document.body.style.backgroundImage = "url('ijstare.jpg')"
+
+var opt1 = document.getElementById('option1');
+	opt1.innerHTML = 'Springen';
+	opt1.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt2 = document.getElementById('option2');
+	opt2.innerHTML = 'Springen';
+	opt2.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt3 = document.getElementById('option3');
+	opt3.innerHTML = 'Springen';
+	opt3.setAttribute("onClick", "javascript:GAMEOVER();");
+}
+function level8() {
+    
+	console.log("Level8")
 	document.getElementById('option1').style.display = "none"
 	document.getElementById('option2').style.display = "none"
 	document.getElementById('option3').style.display = "none"
@@ -93,6 +244,42 @@ function level3() {
 	document.getElementById('level_title').innerHTML = 'Next test'
 	document.body.style.backgroundImage = "url('ijstare.jpg')"
 
+var opt1 = document.getElementById('option1');
+	opt1.innerHTML = 'Springen';
+	opt1.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt2 = document.getElementById('option2');
+	opt2.innerHTML = 'Springen';
+	opt2.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt3 = document.getElementById('option3');
+	opt3.innerHTML = 'Springen';
+	opt3.setAttribute("onClick", "javascript:GAMEOVER();");
 }
+function level9() {
+    
+	console.log("Level9")
+	document.getElementById('option1').style.display = "none"
+	document.getElementById('option2').style.display = "none"
+	document.getElementById('option3').style.display = "none"
+	document.getElementById('opnieuw').style.display = "none"
+    document.getElementById('touw').style.display = "none"
+
+	document.getElementById('level_title').innerHTML = 'Next test'
+	document.body.style.backgroundImage = "url('ijstare.jpg')"
+
+var opt1 = document.getElementById('option1');
+	opt1.innerHTML = 'Springen';
+	opt1.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt2 = document.getElementById('option2');
+	opt2.innerHTML = 'Springen';
+	opt2.setAttribute("onClick", "javascript:GAMEOVER();");
+
+var opt3 = document.getElementById('option3');
+	opt3.innerHTML = 'Springen';
+	opt3.setAttribute("onClick", "javascript:GAMEOVER();");
+}
+
 
     	
